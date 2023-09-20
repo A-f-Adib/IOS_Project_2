446eaf9b2ad8f0b710a69b156f9efa0e8a90bbd7
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-09-20 15:08:35 +0600

    Initial Commit

---
+++ new.SwT/new_SwTApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_SwTApp.swift
+//  new.SwT
+//
+//  Created by A.f. Adib on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct new_SwTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
