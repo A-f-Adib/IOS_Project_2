1cc8592a19a23dac57d91a348395913733a3e348
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-02-04 23:33:35 +0600

    Update RotationEffect.swift

--- new.SwT/Project_17/RotationEffect.swift
+++ new.SwT/Project_17/RotationEffect.swift
@@ -12,7 +12,7 @@ struct RotationEffect: View {
     @State private var finalAmount = Angle.zero
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Taylor Swift")
             .rotationEffect(currentAmount + finalAmount)
             .gesture(
                 RotationGesture()
